698c8231b2b3a4420c58ba40ae616b35be088023
jatin <jatin@captainfresh.in>
2022-06-09 02:15:27 +0530

    Functions: return type

--- Cards/main.go
+++ Cards/main.go
@@ -6,6 +6,13 @@ func main() {
 
 	var card string = "Ace of Spades"
 	NewCard := "Five of Diamonds" // Same As Above
+	NewCardWithFunction := newCardWithFunction()
 	fmt.Println(card)
 	fmt.Println(NewCard)
+	fmt.Println(NewCardWithFunction)
+
+}
+func newCardWithFunction() string {
+	// string literal implies the return type is String
+	return "Ace of Diamonds"
 }
