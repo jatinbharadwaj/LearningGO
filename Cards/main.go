91f4461005ac4a586616daeffe87158bc0bb2696
jatin <jatin@captainfresh.in>
2022-06-09 02:21:03 +0530

    array and slices

--- Cards/main.go
+++ Cards/main.go
@@ -7,6 +7,10 @@ func main() {
 	var card string = "Ace of Spades"
 	NewCard := "Five of Diamonds" // Same As Above
 	NewCardWithFunction := newCardWithFunction()
+
+	cards := []string{card, NewCard, NewCardWithFunction}
+	cards = append(cards, "Six of Spades")
+
 	fmt.Println(card)
 	fmt.Println(NewCard)
 	fmt.Println(NewCardWithFunction)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LearningGO
 Just gonna add some code on GoLang 
 1. Cards- A Card Game with Go (Started on 09-June-2022)
+|- String, Functions 
+|--Arrays and Slice(A grow and shrinkable array) 
