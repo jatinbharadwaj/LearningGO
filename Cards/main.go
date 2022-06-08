f2b05e65ccddc4e3073bf50bf7cc797053e1c381
jatin <jatin@captainfresh.in>
2022-06-09 02:11:18 +0530

    Variables In Go

---
+++ Cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var card string = "Ace of Spades"
+	NewCard := "Five of Diamonds" // Same As Above
+	fmt.Println(card)
+	fmt.Println(NewCard)
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LearningGO
 Just gonna add some code on GoLang 
+1. Cards- A Card Game with Go (Started on 09-June-2022)
